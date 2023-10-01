6660e7bc612eed7f9126532f5b036b810e3a9226
Kovercodes <sas.gta@ya.ru>
2023-10-01 23:26:18 +0500

    feat: welcome page up to login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import "./App.scss";
+import "./styles/App.scss";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/home";
-import Welcome from "./pages/welcome";
+import Home from "./pages/home/home.jsx";
+import Welcome from "./pages/welcome/welcome.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={Home} />
-        <Route path="/welcome" element={Welcome} />
+        <Route path="/" element={<Home />} />
+        <Route path="/welcome" element={<Welcome />} />
       </Routes>
     </BrowserRouter>
   );
