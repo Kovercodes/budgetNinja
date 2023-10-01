3508f4e626c4d861766ed036d4303bbc4a094866
Kovercodes <sas.gta@ya.ru>
2023-10-01 22:32:59 +0500

    pages and font

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import "./App.css";
+import "./App.scss";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/home";
+import Welcome from "./pages/welcome";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={Home} />
+        <Route path="/welcome" element={Welcome} />
+      </Routes>
     </BrowserRouter>
   );
 }
